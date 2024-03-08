953606ccfb6b6b226734590e89a3b73a32a77a41
AlexsandroFde <alexsandro.ferreira02@aluno.ifce.edu.br>
2024-03-07 23:57:41 -0300

    added update functions

---
+++ functions/update.js
@@ -0,0 +1,33 @@
+function formatCounter(value) {
+    return value.toLocaleString();
+  }
+  
+  function updateCounterFormat() {
+    counterElement.textContent = formatCounter(counter);
+  }
+  
+  setInterval(updateCounterFormat, 1000);
+  
+  function updateIncrementButtonColor() { 
+      if (counter < 0) { 
+          incrementBtn.style.backgroundColor = "#FF0000"; 
+      } else if (counter > 0) { 
+          incrementBtn.style.backgroundColor = "#00FF00"; 
+      } else { 
+          incrementBtn.style.backgroundColor = "#FFFF00";
+      } 
+  }
+  
+  setInterval(updateIncrementButtonColor, 100);
+  
+  function updateDecrementButtonColor() { 
+      if (counter < 0) { 
+          decrementBtn.style.backgroundColor = "#FF0000"; 
+      } else if (counter > 0) { 
+          decrementBtn.style.backgroundColor = "#00FF00"; 
+      } else { 
+          decrementBtn.style.backgroundColor = "#FFFF00";
+      } 
+  }
+  
+  setInterval(updateDecrementButtonColor, 100);
